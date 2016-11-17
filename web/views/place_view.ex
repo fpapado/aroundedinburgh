334dc1553bd667413ccbeb7e75e70b62512f8efe
fpapado <fgpapado@gmail.com>
2016-11-17 22:31:11 +0000

    Initial map of category id to colour in view

--- web/views/place_view.ex
+++ web/views/place_view.ex
@@ -1,3 +1,15 @@
 defmodule Rumbl.PlaceView do
   use Rumbl.Web, :view
+  alias Rumbl.Place
+
+  def category_as_colour(%Place{category_id: category_id}) do
+      case category_id do
+          1 -> 'bright-green'
+          2 -> 'pale-red'
+          3 -> 'light-blue'
+          4 -> 'dark-red'
+          5 -> 'gold'
+          _ -> 'gold'
+      end
+  end
 end
