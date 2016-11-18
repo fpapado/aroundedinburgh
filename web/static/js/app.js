8375d874f4101c27d5e205eb689daafb526a6c6a
fpapado <fgpapado@gmail.com>
2016-11-18 21:31:30 +0000

    Factor out map, init based on data attr per place

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,17 +12,12 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 // import 'phoenix_html';
-// import Map from './map';
+import Map from './map';
 
-let placemap = L.map('mapid').setView([55.942456, -3.182448], 14);
+let map = document.getElementById('placemap');
 
-// L.tileLayer('https://api.mapbox.com/styles/v1/fpapado/civo3gxei00472joilop02agr/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v8/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'your.mapbox.project.id',
-    accessToken: 'pk.eyJ1IjoiZnBhcGFkbyIsImEiOiJjaXZudmtvbXQwMDBjMnp0ZWR6NjE4ZTFhIn0.s4SzhiEbl9tkhkkTBmSzjA',
-}).addTo(placemap);
-
-let placemarker = L.marker([55.942456, -3.182448]).addTo(placemap)
-placemarker.bindPopup('Filament Coffee', {className: 'pop'}).openPopup();
+if(map) {
+    console.log(map);
+    console.log(Map);
+    Map.init(map.id, map.getAttribute('data-name'), map.getAttribute('data-lat'), map.getAttribute('data-lng'));
+}
--- web/static/js/map.js
+++ web/static/js/map.js
@@ -1,4 +1,20 @@
-export default Map;
-
 let Map = {
+    init(mapId, placeTitle, lat, lng) {
+        let placemap = L.map(mapId).setView([lat, lng], 14);
+
+        // L.tileLayer('https://api.mapbox.com/styles/v1/fpapado/civo3gxei00472joilop02agr/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v8/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'your.mapbox.project.id',
+            accessToken: 'pk.eyJ1IjoiZnBhcGFkbyIsImEiOiJjaXZudmtvbXQwMDBjMnp0ZWR6NjE4ZTFhIn0.s4SzhiEbl9tkhkkTBmSzjA',
+        }).addTo(placemap);
+
+        let placemarker = L.marker([55.942456, -3.182448]).addTo(placemap);
+        placemarker
+            .bindPopup(placeTitle, {className: 'pop'})
+            .openPopup();
+    },
 };
+
+export default Map;
