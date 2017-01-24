2488763f4930a70e7e7c021a1fd413b62bf618e3
fpapado <fgpapado@gmail.com>
2017-01-24 15:03:21 +0000

    Add secret keys from ENV var

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,13 @@ use Mix.Config
 config :rumbl, Rumbl.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
+
+config :rumbl, Rumbl.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL")
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
