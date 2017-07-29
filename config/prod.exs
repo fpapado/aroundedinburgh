ef2e99f1cab50472cd6b800a40ef9f4cdbc0ca76
fpapado <fgpapado@gmail.com>
2017-07-29 12:08:09 +0100

    Bump phoenix to 1.3

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :rumbl, Rumbl.Endpoint,
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # url: [host: "floating-mesa-61234.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
